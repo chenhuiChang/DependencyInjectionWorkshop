b25b0b06d5b64ff66ca86bff0b465c3592e6aeed
Alex Chang <j4demacia@gmail.com>
2023-01-29 11:12:53 +0800

    feat: get password from db, and hash password

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,10 +1,37 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
         public bool IsValid(string account, string password)
         {
-            return true;
+            string passwordFromDb;
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                passwordFromDb = connection.Query<string>("spGetUserPassword", new { Id = account }, commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var sha256Managed = new SHA256Managed();
+            var stringBuilder = new StringBuilder();
+            var crypto = sha256Managed.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                stringBuilder.Append(theByte.ToString("x2"));
+            }
+            var hashedPassword = stringBuilder.ToString();
+            
+            if (hashedPassword == passwordFromDb)
+            {
+                return true;
+            }
+            
+            return false;
         }
     }
 }
