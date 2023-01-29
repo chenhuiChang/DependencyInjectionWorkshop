8649bb9b3617f8984ccf0b42c38d2069ecceb62f
Alex Chang <j4demacia@gmail.com>
2023-01-29 11:26:53 +0800

    feat: send slack notification when invalid

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using Dapper;
+using SlackAPI;
 
 namespace DependencyInjectionWorkshop.Models
 {
@@ -42,6 +43,9 @@ namespace DependencyInjectionWorkshop.Models
                 return true;
             }
             
+            var slackClient = new SlackClient("my slack api key");
+            string message = $"account:{account} try to login failed";
+            slackClient.PostMessage(response1 => { }, "my channel id", message, "my bot name");
             return false;
         }
     }
